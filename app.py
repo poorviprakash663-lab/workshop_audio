bfbc129cf5c201457c7a1ff54c5eb07e161a081c
poorviprakash663-lab <poorviprakash663@gmail.com>
2026-05-08 15:44:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 def main():
     st.set_page_config(page_title="PragyanAI - VVIET Multimedia Hub", layout="wide")
-    st.image("PragyanAI_Transperent.png")
+    st.image("WhatsApp Image 2026-04-15 at 6.42.56 PM.jpeg")
     st.title(" PragyanAI Multi-Functional Media Hub")
     
     # Create Tabs for Video, YouTube, and PDF
